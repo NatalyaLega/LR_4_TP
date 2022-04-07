e9c5ee68f69e738f0af1693b80c885ec516213f2
NatalyaLega <lega.nata@bk.ru>
2022-04-07 15:03:32 +0300

    lr-4

--- LR_4_TP/Main.cpp
+++ LR_4_TP/Main.cpp
@@ -5,7 +5,7 @@
 #include"Queue.h"
 using namespace std;
 
-
+//функции для 1го задания
 template<class T>
 void sort_direct_merge(T a[], int fsize)  // шаблонная функция сортировки слиянием
 {
@@ -22,7 +22,7 @@ void sort_direct_merge(T a[], int fsize)  // шаблонная функция сортировки слияни
     while (idl < fsize / 2) buf[idbuf++] = a[idl++];
     while (idr < fsize) buf[idbuf++] = a[idr++];
     for (idl = 0; idl < fsize; idl++)a[idl] = buf[idl];
-   delete[]buf;
+    delete[]buf;
 }
 
 
@@ -32,7 +32,7 @@ void mergeSort(T* array, int fsize) //шаблонная функция инверсии массива
     int i = 0;
     cout << "Размер массива: ";
     cout << fsize << endl; //выводим размер массива
-    
+
     cout << "Заданный массив:" << endl;
     for (i = 0; i < fsize - 1; i++) //выводим массив
         cout << array[i] << " -> ";
@@ -62,201 +62,136 @@ void mergeSort(T* array, int fsize) //шаблонная функция инверсии массива
     cout << endl;
 }
 
-//функция, для работы с очередью(а имеено, чтобы вызывать её в основном свиче)->тут прописаны основные операции
-template <class T, typename TT>
-void work_with_queue(T* stptr, TT x)
-{
-	int choise, size, exit = 1;
-	//TT element;
-	
-	try
-	{
-		cout << "Введите размер очереди(от 0 до 50):\n--> ";
-		cin >> size;
-		if (size <= 0)
-			throw "Размер очереди не может быть нулевым или отрицательным!";
-		if (size > 50)
-			throw "Размер очереди больше 50 - это недопустимо!";
-	}
-	catch (const char* ex) { cout << ex << endl; }
-
-	T q;
 
 
-	system("cls");
-
-	while (exit == 1)
-	{
-		cout << "Доступно несколько операций. Выберете, что Вам необходимо: " << endl;
-		cout << "1 - Добавить элемент в очередь" << endl;
-		cout << "2 - Удалить элемент из очереди" << endl;
-		cout << "3 - Проверить очередь на пустоту" << endl;
-		cout << "4 - Напечатать очередь" << endl;
-		cout << "0 - Выйти к выбору задания" << endl;
-		cin >> choise;
-
-		system("cls");
-
-		switch (choise)
-		{
-		case 1:
-			int elem;
-			cout << "Введите элемент стека, который хотите добавить: ";
-			cin >> elem;
-			q = q + elem;
-			//q.print();
-
-			system("pause");
-			cout << "\n";
-			break;
-		case 2:
-			try
-			{
-			
-
-			}
-			catch (const char* ex) { cout << ex << endl; }
-			system("pause");
-			break;
-		case 3:
-			
+template <typename T>
+void fillArrayWithRandomNumbers(T* array, int size);
 
 
-		case 4:
-			
-			//q.print();
-			//как-то вызвать метод принт
-			cout << "\n";
-			system("pause");
-			break;
-		case 0:
-			delete[] stptr;
-			exit = 0;
-			break;
-		}
-	}
+template <>
+void fillArrayWithRandomNumbers(float* array, int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 51;
+        array[i] += (float)(rand() % 100) / (float)100;
+    }
 }
+
+template <>
+void fillArrayWithRandomNumbers(double* array, int size) {
+    for (int i = 0; i < size; i++) {
+        array[i] = rand() % 51;
+        array[i] += (double)(rand() % 100) / (double)100;
+    }
+}
+
 
 int main()
 {
-	setlocale(LC_ALL, "Rus");
+    setlocale(LC_ALL, "Rus");
 
-	//переменные для 1го задания
-	int size_array; //размер введённого массива
-	int* mass1 = 0;
-	float* mass2 = 0;
-	double* mass3 = 0;
+    //переменные для 1го задания
+    int size_array; //размер введённого массива
+    int* mass1 = 0;
+    float* mass2 = 0;
+    double* mass3 = 0;
 
-	//переменные для 2го задания
-	Queue<int>* queue1 = nullptr;
-	int x1 = 0;
-	Queue<float>* queue2 = nullptr;
-	float x2 = 0;
-	Queue<double>* queue3 = nullptr;
-	double x3 = 0;
+    int choice, choice_type, choice_type2, exit = 1, exit1 = 1; //для работы с меню
 
-	int choice, choice_type, exit = 1, exit1 = 1; //для работы с меню
+    while (exit == 1)
+    {
+        cout << "Выберите номер задания: " << endl;
+        cout << "1 - Задание 1 - Сортировка массива\n2 - Задание 2 - Параметризированный класс циклической очереди\n0 - Выход\n-->" << endl;
+        cin >> choice;
+        system("cls");
+        switch (choice)
+        {
+        case 1:  ////////// ЗАДАНИЕ_1 ///////////
+            while (exit1 == 1)
+            {
+                cout << "Выберите желаемый тип данных массива: " << endl;
+                cout << "1 - int\n2 - float\n3 - double\n0 - Выход --> " << endl;
+                cin >> choice_type;
 
-	while (exit == 1)
-	{
-		cout << "Выберите номер задания: " << endl;
-		cout << "1 - Задание 1 - Сортировка массива\n2 - Задание 2 - Параметризированный класс циклической очереди\n0 - Выход\n-->" << endl;
-		cin >> choice;
-		system("cls");
-		switch (choice)
-		{
-		case 1:  ////////// ЗАДАНИЕ_1 ///////////
-			while (exit1 == 1)
-			{
-				cout << "Выберите желаемый тип данных массива: " << endl;
-				cout << "1 - int\n2 - float\n3 - double\n0 - Выход --> " << endl;
-				cin >> choice_type;
+                switch (choice_type)
+                {
+                case 1:
+                    cout << "Введите размер массива: " << endl;
+                    cin >> size_array;
+                    mass1 = new int[size_array]; //выделяем память под массив
 
-				switch (choice_type)
-				{
-				case 1:
-					cout << "Введите размер массива: " << endl;
-					cin >> size_array;
-					mass1 = new int[size_array]; //выделяем память под массив
+                    for (int i = 0; i < size_array; i++) //заполняем массив
+                        mass1[i] = rand() % 20;
 
-					for (int i = 0; i < size_array; i++) //заполняем массив
-						mass1[i] = rand() % 20;
+                    mergeSort(mass1, size_array); //вызываем функцию сортировка
+                    delete[] mass1;
+                    system("pause");
+                    system("cls");
+                    break;
+                case 2:
+                    cout << "Введите размер массива: " << endl;
+                    cin >> size_array;
+                    mass2 = new float[size_array]; //выделяем память под массив
 
-					mergeSort(mass1, size_array); //вызываем функцию сортировка
-					delete[] mass1;
-					system("pause");
-					system("cls");
-					break;
-				case 2:
-					cout << "Введите размер массива: " << endl;
-					cin >> size_array;
-					mass2 = new float[size_array]; //выделяем память под массив
+                    for (int i = 0; i < size_array; i++) //заполняем массив
+                        mass2[i] = (-60.0 + rand() % 119) / (2.0 + rand() % 2);
 
-					for (int i = 0; i < size_array; i++) //заполняем массив
-						mass2[i] = (-60.0 + rand() % 119) / (2.0 + rand() % 2);
+                    mergeSort(mass2, size_array); //вызываем функцию сортировка
+                    delete[] mass2;
+                    system("pause");
+                    system("cls");
+                    break;
+                case 3:
+                    cout << "Введите размер массива: " << endl;
+                    cin >> size_array;
+                    mass3 = new double[size_array]; //выделяем память под массив
 
-					mergeSort(mass2, size_array); //вызываем функцию сортировка
-					delete[] mass2;
-					system("pause");
-					system("cls");
-					break;
-				case 3:
-					cout << "Введите размер массива: " << endl;
-					cin >> size_array;
-					mass3 = new double[size_array]; //выделяем память под массив
+                    for (int i = 0; i < size_array; i++) //заполняем массив
+                        mass3[i] = (-60.0 + rand() % 119) / (2.0 + rand() % 2);
 
-					for (int i = 0; i < size_array; i++) //заполняем массив
-						mass3[i] = (-60.0 + rand() % 119) / (2.0 + rand() % 2);
+                    mergeSort(mass3, size_array); //вызываем функцию сортировка
+                    delete[] mass3;
+                    system("pause");
+                    system("cls");
+                    break;
 
-					mergeSort(mass3, size_array); //вызываем функцию сортировка
-					delete[] mass3;
-					system("pause");
-					system("cls");
-					break;
-				
-				case 0:
-					exit1 = 0;
-					break;
-				default:
-					break;
-				}
-			}
-			break;
-		case 2: ///////// ЗАДАНИЕ_2 //////////
-			system("cls");
-			cout << "Выберите желаемый тип данных массива: " << endl;
-			cout << "1 - int\n2 - float\n3 - double\n0 - Выход к выбору задания\n-->" << endl;
-			cin >> choice_type;
-			switch (choice_type)
-			{
-			case 1:
-				work_with_queue(queue1, x1);
-				system("pause");
-				system("cls");
-				break;
-			case 2:
-				work_with_queue(queue2, x2);
-				system("pause");
-				system("cls");
-				break;
-			case 3:
-				work_with_queue(queue3, x3);
-				system("pause");
-				system("cls");
-				break;
-			case 0:
-				break;
-			default:
-				break;
-			}
-			break;
-		case 0:
-			exit = 0;
-			break;
-		default:
-			exit = 0;
-			break;
-		}
-	}
-	return 0;
+                case 0:
+                    exit1 = 0;
+                    break;
+                default:
+                    break;
+                }
+            }
+            break;
+        case 2: ///////// ЗАДАНИЕ_2 //////////
+            cout << "Выберите желаемый тип данных массива: " << endl;
+            cout << "1 - Int\n2 - Float\n3 - Double\n0 - Выход к выбору задания\n-->" << endl;
+            cin >> choice_type2;
+            system("cls");
+            switch (choice_type2)
+            {
+            case '1': {
+                cout << "case1";//не заходит в свич
+                CyclicQueue<int> queue;
+                queue.queueMenu();
+                break; }
+            case '2': {
+                CyclicQueue<float> queue;
+                queue.queueMenu();
+                break; }
+            case '3': {
+                CyclicQueue<double> queue;
+                queue.queueMenu();
+                break; }
+            case '0': {
+                choice_type = 0;
+                break; }
+            default: {
+                choice_type = 0;
+                cout << "Wrong key!!!";
+                break; }
+            }
+        }
+    }
+    return 0;
 }
+
--- LR_4_TP/Queue.h
+++ LR_4_TP/Queue.h
@@ -1,101 +1,278 @@
-#pragma once
-#include "Queue_elem.h"
-using namespace std;
-template <class T> 
-class Queue
-{
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <random>
+#include <exception>
+#define _CRT_SECURE_NO_WARNINGS
 
-	T* q; //указатель на тип T
-	int sloc, rloc; //переменные
-	int length;
-public:
-	Queue(int size) //конструктор
-	{
-		size++;
-		q = new T[size]; //создали массив в динамической памяти
-		if (!q)
-		{
-			cout << "Nevozmoghno sozdaty ocheredy. \n";
-			exit(1);
-		}
-		length = size; //длина массива на единицу больше чем длина очереди;сохранить в length
-		sloc = rloc = 0;
-	}
-	Queue() : length(0.0) {}; //конструктор без параметров (исп. список инициализации); 
-	~Queue() { delete[] q; } //деструктор 
-	
-	void operator+ (Queue<T>& r) //оператор добавления  
-	{
-		if (sloc == length || (sloc == length && !rloc))
-		{
-			cout << "Ocheredy zapolnena.\n";
-			return;
-		}
-		q[sloc] = r;
-		sloc++;
-		if (sloc == length) sloc = 0; //циклический переход
-	}
-	void operator- () //оператор извлечения
-	{
-		if (rloc == length) rloc = 0;//циклический переход
-		if (rloc == sloc)
-		{
-			cout << "Ocheredy pusta.\n";
-			return 0;
-		}
-		rloc++;
-		return q[rloc - 1];
-	}
-	bool operator! () const //проверка на пустоту
-	{
-		if (length == 0)
-			return true;
-		else
-			return false;
-	}
-	//геттеры и сеттеры
-	int get_size() const //взять размер стека
-	{
-		return length;
-	}
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::numeric_limits;
+using std::streamsize;
 
-	
-	void print() {
-		if (size == 0)
-		{
-			cout << "Список пуст, выводить нечего." << endl;
-		}
-		else
-	    {
-		for (int ix = rloc; ix >= sloc; ix--)
-			cout << q[ix] << " ";
-		cout << endl;
-	    }
-	}
+int enterUint() {
 
-	//void print()
-	//{
-	//	try
-	//	{
-	//		if (!this)
-	//			throw "Стек пуст, выводить нечего.\n";
+    int num;
 
-	//		Queue_elem<T>* temp = q;
-	//		if (q == 0)
-	//			throw "Удалять нечего, стек и так пуст!";
-	//		while (temp->get_next() != nullptr)
-	//		{
-	//			std::cout << temp->get_data() << " <-- ";
-	//			temp = temp->get_next();
-	//		}
-	//		std::cout << temp->get_data() << std::endl;
-	//	}
-	//	catch (const char* ex)
-	//	{
-	//		std::cout << ex << std::endl;
-	//	}
-	//	//вывод стека на экран
-	//}
+    cout << "Enter usigned int number" << endl;
+    cin >> num;
+
+    while (!(cin.good()) || num < 0) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Wrong number! Try once again!" << endl;
+        cout << "Enter unsigned int number" << endl;
+        cin >> num;
+    }
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    return num;
+}
+
+template <typename T>
+class CyclicQueue;
+
+
+template <typename T>
+class QueueElement {
+    T element;
+    QueueElement<T>* ptrNext = nullptr;
+    friend class CyclicQueue<T>;
 };
-	
-	
+
+
+template <typename T>
+class CyclicQueue {
+public:
+    CyclicQueue();
+    ~CyclicQueue();
+    void printQueue();
+    void queueMenu();
+    CyclicQueue<T> operator+(QueueElement<T>* newElement);
+    CyclicQueue<T> operator+(int ignored);
+    CyclicQueue<T> operator-(int ignored);
+    bool operator!();
+private:
+    QueueElement<T>* ptrFirst = nullptr;
+    QueueElement<T>* ptrLast = nullptr;
+    int amountElements = 0;
+    T enterValue();
+    friend int enterUint();
+    T createRandomValue();
+
+};
+
+template <typename T>
+CyclicQueue<T>::~CyclicQueue() {
+
+    QueueElement<T>* delElement = ptrFirst, * buffElement;
+
+    while (delElement != ptrLast) {
+        buffElement = delElement->ptrNext;
+        delete delElement;
+        delElement = buffElement;
+    }
+
+    delete ptrLast;
+}
+
+
+
+template <typename T>
+CyclicQueue<T>::CyclicQueue() {
+
+    cout << "Enter size of queue: " << endl;
+    int num = enterUint();
+
+    QueueElement<T>* newElement;
+
+    for (int i = 0; i < num; i++) {
+        newElement = new QueueElement<T>;
+        newElement->element = createRandomValue();
+        *this + newElement;
+    }
+
+}
+
+
+template <typename T>
+void CyclicQueue<T>::printQueue() {
+
+    QueueElement<T>* element = ptrFirst;
+
+    while (element != ptrLast)
+        cout << element->element << "<-";
+
+    cout << ptrLast->element;
+}
+
+template <typename T>
+T CyclicQueue<T>::enterValue() {
+
+    T value;
+
+    cout << "Enter value" << endl;
+    cin >> value;
+
+    while (!(cin.good())) {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Wrong value! Try once again!" << endl;
+        cout << "Enter value" << endl;
+        cin >> value;
+    }
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    return value;
+
+}
+
+
+template <typename T>
+T CyclicQueue<T>::createRandomValue() {
+
+    return rand() % 51;
+}
+
+template <typename T>
+void CyclicQueue<T>::queueMenu() {
+
+    while (1) {
+        cout << "1 - add element to queue" << endl << "2 - delete element from queue" << endl << "3 - check if queue is empty" <<
+            endl << "4 - print queue" << endl << "b - go back" << endl;
+        char c;
+        cin >> c;
+        switch (c) {
+        case '1':
+            *this + 0;
+            break;
+        case '2':
+            try {
+                *this - 0;
+            }
+            catch (std::invalid_argument) {
+                cout << "Nothing to delete!" << endl;
+            }
+            break;
+        case '3':
+            cout << !(*this) << endl;
+            break;
+        case '4':
+            printQueue();
+            break;
+        case 'b':
+            return;
+        default:
+            cout << "Wrong key" << endl;
+        }
+    }
+}
+
+template <typename T>
+CyclicQueue<T> CyclicQueue<T>::operator+(int ignored) {
+
+    QueueElement<T>* newElement = new QueueElement<T>;
+    newElement->element = enterValue();
+
+    if (ptrFirst == nullptr) {
+
+        newElement->ptrNext = newElement;
+
+        ptrFirst = newElement;
+        ptrLast = newElement;
+
+        cout << "Added" << endl;
+        amountElements++;
+        return *this;
+    }
+
+    if (ptrFirst == ptrLast) {
+
+        newElement->ptrNext = ptrFirst;
+        ptrFirst->ptrNext = newElement;
+
+        ptrLast = newElement;
+
+        cout << "Added" << endl;
+        amountElements++;
+        return *this;
+    }
+
+    newElement->ptrNext = ptrFirst;
+    ptrLast->ptrNext = newElement;
+    ptrLast = newElement;
+    amountElements++;
+
+    cout << "Added" << endl;
+    return *this;
+}
+
+template <typename T>
+CyclicQueue<T> CyclicQueue<T>::operator+(QueueElement<T>* newElement) {
+
+    if (ptrFirst == nullptr) {
+
+        newElement->ptrNext = newElement;
+
+        ptrFirst = newElement;
+        ptrLast = newElement;
+
+        amountElements++;
+        return *this;
+    }
+
+    if (ptrFirst == ptrLast) {
+
+        newElement->ptrNext = ptrFirst;
+        ptrFirst->ptrNext = newElement;
+
+        ptrLast = newElement;
+
+        amountElements++;
+        return *this;
+    }
+
+    newElement->ptrNext = ptrLast;
+    ptrFirst->ptrNext = newElement;
+    ptrLast = newElement;
+    amountElements++;
+    return *this;
+
+}
+
+template <typename T>
+CyclicQueue<T> CyclicQueue<T>::operator-(int ignored) {
+
+    if (ptrFirst == nullptr)
+        throw std::invalid_argument("No items in queue");
+
+    if (ptrFirst == ptrLast) {
+
+        delete ptrFirst;
+        amountElements--;
+
+        ptrFirst = nullptr;
+        ptrLast = nullptr;
+
+        cout << "Deleted" << endl;
+        return *this;
+    }
+
+    QueueElement<T>* delElement = ptrFirst;
+    ptrFirst = ptrFirst->ptrNext;
+    ptrLast->ptrNext = ptrFirst;
+    amountElements--;
+
+    delete delElement;
+    return *this;
+}
+
+template <typename T>
+bool CyclicQueue<T>::operator!() {
+
+    return amountElements == 0;
+}
